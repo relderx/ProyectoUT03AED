dad1e4efa4f4c38b029b9f78cfc9bbf172a7b6fd
relderx <samuel.guancha@gmail.com>
2024-12-13 08:58:12 +0000

    Cambios en nombres de botones

--- views/movimiento_view/src/main.py
+++ views/movimiento_view/src/main.py
@@ -1,26 +1,73 @@
 import flet as ft
-
+# from flet import navigation
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+    page.title = "Movimiento de Inventario"
+    page.window_width = 1920
+    page.window_height = 1080
+    page.bgcolor = ft.colors.WHITE
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
+    encabezado = ft.Row([
+        ft.Text("Movimiento de Inventario", size=30, weight=ft.FontWeight.BOLD, text_align=ft.TextAlign.LEFT),
+        ft.Row(
+            [
+                ft.ElevatedButton("Página Principal", width=150),
+                ft.ElevatedButton("Pedidos", width=150)
+            ],
+            alignment=ft.MainAxisAlignment.END,
+            expand=True
         )
+    ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
+
+        # Botones inferiores
+    botones_inferiores = ft.Row([
+        ft.ElevatedButton("Borrar", width=100, disabled=True),
+        ft.ElevatedButton("Insertar", width=100),
+        ft.ElevatedButton("Modificar", width=100, disabled=True),
+    ], alignment=ft.MainAxisAlignment.END)
+
+    # Tabla de productos
+    encabezados_tabla = ["Producto", "Tipo de Movimiento", "Cantidad", "Fecha", "Comentario"]
+    datos_tabla = [
+        ["Producto1", "Entrada", "2", "", ""],
+        ["Producto2", "Salida", "5", "", ""],
+        ["Producto3", "", "1", "", ""],
+    ]
+
+    tabla = ft.DataTable(
+        width=1920,
+        border_radius=2,
+        border=ft.border.all(2, "red"),
+        horizontal_lines=ft.BorderSide(2, "blue"),
+        vertical_lines=ft.BorderSide(2, "blue"),
+        columns=[ft.DataColumn(ft.Text(encabezado)) for encabezado in encabezados_tabla],
+        rows=[
+            ft.DataRow(
+                cells=[ft.DataCell(ft.Text(dato)) for dato in fila]
+            ) for fila in datos_tabla
+        ],
+
     )
 
+    # Campo de búsqueda
+    buscar_filtro = ft.Row([
+        ft.TextField(label="Buscar", width=200),
+        ft.IconButton(icon=ft.icons.FILTER_LIST),
+    ], alignment=ft.MainAxisAlignment.END)
 
-ft.app(main)
+    # Estructura de la página
+    page.add(
+        encabezado, 
+        # botones_superiores,
+
+        botones_inferiores,
+        ft.Divider(),
+        ft.Text("Productos", size=20, weight=ft.FontWeight.BOLD),
+        buscar_filtro,
+        tabla,
+
+        ft.Divider(),
+  
+    )
+
+ft.app(target=main)

b62f6c02a43ee6361da1ba33cdb4e28f08a0d0ca
Alejandro Hernández León <alehleon2001@gmail.com>
2025-01-11 21:07:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,6 @@
 import flet as ft
-from fastapi import FastAPI
-import uvicorn
-import threading
- 
-# from views.movimiento_view.src.main import main as create_movimiento_page
-# from views.pedido_view.src.main import main as create_pedido_page
+
 from views.producto_view.src.main import main as create_producto_page
 
-# Crear la app FastAPI
-app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "¡Hola desde FastAPI!"}
-
-def run_fastapi():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
 if __name__ == "__main__":
-    # Ejecutar FastAPI en un hilo
-    threading.Thread(target=run_fastapi).start()
-
-    # Ejecutar Flet en tu dirección IP local
-    ft.app(target=create_producto_page, view=ft.AppView.WEB_BROWSER, host="0.0.0.0", port=80)
+    ft.app(target=create_producto_page, view=ft.AppView.WEB_BROWSER, host='', port=80)

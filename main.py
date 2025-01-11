ba428bec51b8d11e4e114d5acee7c4f6e9db3f29
Alejandro Hernández León <alehleon2001@gmail.com>
2025-01-11 21:53:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import flet as ft
+from store import store_view  # Importas la vista desde otro archivo
 
 def main(page: ft.Page):
-    page.title = "Routes Example"
+    page.title = 'Sistema de gestión de inventario'
+    page.theme_mode = 'light'
 
     def route_change(route):
         page.views.clear()
-        page.views.append(
-            ft.View(
-                "/",
-                [
-                    ft.AppBar(title=ft.Text("Flet app"), bgcolor=ft.colors.SURFACE_VARIANT),
-                    ft.ElevatedButton("Visit Store", on_click=lambda _: page.go("/store")),
-                ],
-            )
-        )
-        if page.route == "/store":
+
+        # Vista principal
+        if page.route == '/':
             page.views.append(
                 ft.View(
-                    "/store",
+                    '/',
                     [
-                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
-                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        ft.AppBar(
+                            title=ft.Text('Sistema de gestión de inventario'),
+                            bgcolor=ft.colors.SURFACE_VARIANT,
+                            center_title=True,
+                            leading=ft.IconButton(ft.icons.HOME, on_click=lambda _: page.go('/')),
+                        ),
+                        ft.ElevatedButton('Visit Store', on_click=lambda _: page.go('/store')),
                     ],
                 )
             )
+
+        # Vista de la tienda
+        if page.route == '/store':
+            page.views.append(store_view(page))  # Llamas a la vista desde el archivo 'store.py'
+        
         page.update()
 
     def view_pop(view):
@@ -35,5 +40,5 @@ def main(page: ft.Page):
     page.on_view_pop = view_pop
     page.go(page.route)
 
-
-ft.app(main, view=ft.AppView.WEB_BROWSER)
+if __name__ == '__main__':
+    'ft'.app(target=main, view=ft.AppView.WEB_BROWSER, host='', port=80)
